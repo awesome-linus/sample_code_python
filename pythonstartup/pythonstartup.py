f051e706d8afcdda1425bb914c9d42cb62309f44
awesome_linus <awesomeblog.linus@gmail.com>
2017-11-13 22:07:56 +0900

    add pythonstartup

---
+++ pythonstartup/pythonstartup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# python startup
+import readline
+import rlcompleter
+import atexit
+import os
+
+# complete tab complement
+readline.parse_and_bind('tab: complete')
+
+histfile = os.path.join(os.environ['HOME'], '.pythonhistory')
+try:
+    readline.read_history_file(histfile)
+except IOError:
+    pass
+atexit.register(readline.write_history_file, histfile)
+del os, histfile, readline, rlcompleter, atexit
